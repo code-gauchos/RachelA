6db9b12c567fbd8899a9248bc8a915db6286cca0
unknown <rach.kukai@gmail.com>
2018-08-29 18:56:39 -0700

    initialcheckin

---
+++ Thankyou.py
@@ -0,0 +1,9 @@
+myName = "Rachel"
+
+myAge = 19
+
+yourName = input("What is your name?")
+
+yourAge = input("How old are you?")
+
+print("my name is" , myName, "and I am" , myAge , "years old")[]
---
+++ squarespiral.py
@@ -0,0 +1,12 @@
+import turtle 
+
+TurtlePen = turtle.Pen()
+
+for x in range(100):
+    TurtlePen.forward(x)
+    TurtlePen.left(90)
+    TurtlePen.right(40)
+    TurtlePen.circle
+    
+
+
